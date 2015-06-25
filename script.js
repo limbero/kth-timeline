d56e288b4509c58632f339a4ab32e0476df5eb38
Axel Samuelsson <axelsam@kth.se>
2015-06-26 01:26:29 +0200

    whoops

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function parseJSON (data) {
 
 function requestedResponse () {
     config = JSON.parse(this.responseText)
+
+    var script = document.createElement('script')
+    script.src = '//spreadsheets.google.com/feeds/list/'+config.spreadsheet+'/'+config.sheet[0]+'/public/values?alt=json-in-script&callback=parseJSON'
+
+    document.head.appendChild(script)
 }
 
 var request = new XMLHttpRequest()
 request.onload = requestedResponse
 request.open("get", "config.json", true)
 request.send()
-
-var script = document.createElement('script')
-script.src = '//spreadsheets.google.com/feeds/list/'+config.spreadsheet+'/'+config.sheet[0]+'/public/values?alt=json-in-script&callback=parseJSON'
-
-document.head.appendChild(script)
