9defc6ad4f592aa43f201d165a93af1bc14dc85c
Axel Samuelsson <axelsam@kth.se>
2015-06-26 01:23:24 +0200

    still rusty, now JSONP

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// function parseJSON (data) {
-// }
+function parseJSON (data) {
+    console.log(data)
+}
 
 function requestedResponse () {
-    console.log(this.responseText)
+    config = JSON.parse(this.responseText)
 }
 
 var request = new XMLHttpRequest()
@@ -10,7 +11,7 @@ request.onload = requestedResponse
 request.open("get", "config.json", true)
 request.send()
 
-// var script = document.createElement('script')
-// script.src = '//spreadsheets.google.com/feeds/list/1hANk7xNMYR-bm8N5X_0TOLyn0X0u64FdgMr9YlyT5LI/o5yxovo/public/values?alt=json-in-script&callback=parseJSON'
-//
-// document.head.appendChild(script)
+var script = document.createElement('script')
+script.src = '//spreadsheets.google.com/feeds/list/'+config.spreadsheet+'/'+config.sheet[0]+'/public/values?alt=json-in-script&callback=parseJSON'
+
+document.head.appendChild(script)
